8d27bf0deca98585e90350017f9f6fa57c4a11f0
jing gong <leogong99@gmail.com>
2022-01-15 23:26:28 -0800

    Add build folder

---
+++ build/precache-manifest.a9a2916607e9d607cd0022dc9581248c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b69cfc3b10dc4098bc4f898fef960d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0df51de97d99213209f",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "5849a7fac4fc5f37e8ec",
+    "url": "/static/css/main.7712e426.chunk.css"
+  },
+  {
+    "revision": "e0df51de97d99213209f",
+    "url": "/static/js/2.d889eb2a.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/static/js/2.d889eb2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5849a7fac4fc5f37e8ec",
+    "url": "/static/js/main.8f5b2e8a.chunk.js"
+  },
+  {
+    "revision": "02811df24c3b1d52a7cf",
+    "url": "/static/js/runtime-main.5f881e2e.js"
+  }
+]);
